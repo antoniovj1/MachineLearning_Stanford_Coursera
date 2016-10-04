b70ba73819c75d7da5828aed53936fa90b232f08
Antonio de la Vega <antoniodelavegajimenez@gmail.com>
2016-10-04 02:20:41 +0200

    closes #14 - Cost function and gradient

--- Exercise 2/ex2/ex2/costFunction.m
+++ Exercise 2/ex2/ex2/costFunction.m
@@ -20,7 +20,7 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
-J = ( -y .* log(sigmoid(X*theta)) - ( 1 - y) .* log(1 - sigmoid(X*theta))) / m;
+J = (1 / m) * sum( -y'*log(sigmoid(X*theta)) - (1-y)'*log( 1 - sigmoid(X*theta)) );
 
 grad = ((sigmoid(X*theta) - y )' * X) / m;
 
